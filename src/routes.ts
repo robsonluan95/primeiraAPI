2a6cbc698224bd14abb118527fa39835ae77cbd8
Robson Luan <rluan95@hotmail.com>
2024-03-23 18:38:57 -0300

    "Att final"

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import {Router,Request,Response} from 'express'
+import {Router,Request,Response, NextFunction} from 'express'
 
 const router = Router();
 
@@ -38,6 +38,31 @@ const carros =[
     
 ]
 
+
+
+router.use((req:Request,res:Response,next:NextFunction)=>{
+    console.log("Passou pelo middleware global")
+
+    return next();
+    
+})
+
+function checkCarro(req:Request,res:Response,next:NextFunction){
+
+    if(!req.body.modelo||!req.body.marca||!req.body.valor){
+        return res.status(400).json({error:"Dados Inválidos!"})
+    }
+    return next()
+}
+function checkCarroOk(req:Request,res:Response,next:NextFunction) {
+    const  carroExistente=carros.find(c=> c.modelo===req.params.modelo);
+
+   if (!carroExistente){
+       return res.status(404).json({ error : "Não encontrado"});
+   }
+
+   return next();
+}   
 // router.get('/tarefas/:id',(req:Request,res:Response)=>{
     
 //     const id=req.params.id;   
@@ -50,16 +75,18 @@ interface Carro{
     valor: string;
 
 }
-const listaCarros :Carro[] = []
+
 //listar todos os carros
 router.get("/carros",(req:Request,res:Response)=>{
-    
+    const listaCarros :Carro[] = []
+
     carros.map((carro)=>{
         if (parseInt(carro.valor)>=1){
             listaCarros.push(carro)
         }
         
     })
+    
     res.json(listaCarros)
 
 })
@@ -75,46 +102,48 @@ router.get("/carro/:modelo",(req:Request,res:Response)=>{
 
 //Rotas para adicionar um valor!
 
-router.post("/carro",(req:Request,res:Response)=>{
+router.post("/carro",checkCarro,(req:Request,res:Response)=>{
 
     const novoCarro:Carro = req.body
-    console.log(typeof(novoCarro));
-
-    if (Object.keys(novoCarro).length===0||novoCarro.marca===""||novoCarro.modelo===""||novoCarro.valor===""){
-        res.status(400).json({message:"Error ao cadastrar!"})
-        return;
-    }
-    
-    
 
     carros.push(novoCarro)
+
     res.json({	message:"Carro adicionado"})
 })
 
 //Atualizar um Item
-router.put("/carro/:modelo",(req:Request,res:Response)=>{
+router.put("/carro/:modelo",checkCarro,checkCarroOk,(req:Request,res:Response)=>{
     const modeloCarro = req.params.modelo
     const {marca,modelo,valor} = req.body
 
-    let carroEncontrado = false
-
-    carros.forEach((carro)=>{
-        if(carro.modelo===modeloCarro){
-            carro.marca= marca || carro.marca
-            carro.modelo= modelo || carro.modelo
-            carro.valor= valor || carro.valor
-            carroEncontrado = true
+    
+    let carroAtualizado=false
+    carros.forEach((carro) => {
+        if (carro.modelo === modeloCarro) {
+            // Atualiza os dados do carro se o modelo corresponder
+            carro.marca = marca || carro.marca;
+            carro.modelo = modelo || carro.modelo;
+            carro.valor = valor || carro.valor;
+            carroAtualizado = true;
         }
     });
 
-    if(!carroEncontrado){
-        return res.status(400).json({mes: "Não encontrou o Carro!"})
-    }  
-    console.log(listaCarros)
-
     
-    res.json({msg:"Atualizado com sucesso!"});
+    
+    if(carroAtualizado){
+        res.json({msg:"Atualizado com sucesso!"});
+    }else{
+        res.status(400).json({message:`Não foi possível atualizar o Carro ${modeloCarro}`})
+    }
 
+}) 
+
+
+router.delete('/carro/:index',checkCarroOk,(req:Request, res:Response)=>{
+
+    const {index} = req.params
+    carros.splice(Number(index),1)
+    res.json({msg:"Deletado com sucesso!"})
 })
 
 export {router};
